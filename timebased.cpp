97d48aa00384bd468a0934485458e1e1ed21c20a
Astr0id <792880387@qq.com>
2016-05-17 15:49:36 +0800

    add overnight function

--- timebased.cpp
+++ timebased.cpp
@@ -18,6 +18,7 @@ void TimeBased()
 	set<string> City(citylist,citylist+sizeof(citylist)/sizeof(*citylist));//set of unvisted nodes
 	map<string,int> V;//result matrix
 	map<string,int> ST;//record the start time
+	map<string,int> OT;//note that if there is an overnight wait
 	map<string,string>prev;//record previous node
 	set<string>::iterator it;//set S or City 's iterator
 	map<string,int>::iterator mapit;//map V 's iterator
@@ -28,6 +29,7 @@ void TimeBased()
 	{
 		V.insert(pair<string,int>(*it,1000));//set every node to start node 's distance is INFINITY
 		ST[*it]=999;//initialize all node's start time to INFINITY
+		OT[*it]=999;//asume all node's not wait till next day
 	}
 	mapit=V.find("A");//A can be replaced as the start city
 	if(mapit!=V.end())mapit->second=0;//Set A to A 's distance to ZERO
@@ -63,11 +65,16 @@ void TimeBased()
 					//ESTABILISH  A  LEGAL ROUTE
 					if(!S.count(Data[i].dest))
 					{
-						if(Data[i].TB[j].arrival<ST[Data[i].dest] && Data[i].TB[j].start >= ST[Data[i].from] )//the shortest available route
+						int est;
+						est=Data[i].TB[j].arrival-ST[Data[i].from];
+						if(Data[i].TB[j].start < ST[Data[i].from])est+=24;
+
+						if(est<OT[Data[i].dest])//the shortest available route
 						{
+							OT[Data[i].dest]=est;
 							ST[Data[i].dest]=Data[i].TB[j].arrival;
 							prev[Data[i].dest]=Data[i].from;
-							V[Data[i].dest]=ST[Data[i].dest]-ST[Data[i].from]+V[prev[Data[i].dest]];//distance + prev point's distance
+							V[Data[i].dest]=OT[Data[i].dest]+V[prev[Data[i].dest]];//distance + prev point's distance
 							//finish updating a route
 							//travel time = ST[dest]-ST[from];
 							//Total time => while tracing back from the dest and cumulate each part's travel time
@@ -76,8 +83,8 @@ void TimeBased()
 							cout<<"Distance is "<<V[Data[i].dest]<<endl;
 						}
 					}
-
 				}
+				
 			}
 		}
 
@@ -135,89 +142,4 @@ void TimeBased()
 		cout<<mapit->first<<" is "<<mapit->second<<endl;
 	}
 	
-}
-
-void MoneyBased()
-{
-	set<string> S;
-	set<string> City(citylist, citylist + sizeof(citylist) / sizeof(*citylist));//set of unvisted nodes
-	map<string, int> V;
-	map<string, int> ST;
-	map<string, int> RM;
-	map<string, string>prev;
-	set<string>::iterator it;
-	map<string, int>::iterator mapit;
-	string minC;
-	int min = 32766;
-	for (it = City.begin(); it != City.end(); it++)
-	{
-		V.insert(pair<string, int>(*it, 32767));
-		RM[*it] = min;
-	}
-	mapit = V.find("A");
-	if (mapit != V.end())mapit->second = 0;
-	S.insert("A");
-	City.erase("A");
-	ST["A"] = 0;
-	RM["A"] = 0;
-	while (!City.empty())
-	{
-		for (int i = 1; i <= routecount; i++)
-		{
-
-			if (S.count(Data[i].from))
-			{
-				for (int j = 0; j<Data[i].rNumber; j++)
-				{
-					if (!S.count(Data[i].dest))
-					{
-						if (Data[i].TB[j].cost<RM[Data[i].dest] && Data[i].TB[j].start >= ST[Data[i].from])
-						{
-							ST[Data[i].dest] = Data[i].TB[j].arrival;
-							prev[Data[i].dest] = Data[i].from;
-							RM[Data[i].dest] = Data[i].TB[j].cost;
-							if (RM[Data[i].dest] + V[prev[Data[i].dest]]<V[Data[i].dest])
-								V[Data[i].dest] = RM[Data[i].dest] + V[prev[Data[i].dest]];//- RM[Data[i].from]
-							cout << "UPDATE Route from " << Data[i].from << " to " << Data[i].dest << endl;
-							cout << "Start at " << Data[i].TB[j].start << " Arive at " << Data[i].TB[j].arrival << endl;
-							cout << "Moneycost is " << V[Data[i].dest] << endl;
-						}
-					}
-				}
-			}
-		}
-		mapit = V.begin();
-		for (; mapit != V.end(); mapit++)
-		{
-
-			if (!S.count(mapit->first))//find a node which isn't belongs to set S
-			{
-				if (V[mapit->first]<min)
-				{
-					minC = mapit->first;
-					min = V[mapit->first];
-				}
-			}
-		}
-		min = 32766;
-		if (City.count(minC))
-		{
-			it = City.find(minC);
-			S.insert(*it);
-			City.erase(*it);
-		}
-		cout << "City include " << endl;
-		for (it = City.begin(); it != City.end(); it++) //initialize
-		{
-			cout << *it << " ";//set every node to start node 's distance is INFINITY
-		}
-		cout << endl;
-	}
-	system("CLS");
-	mapit = V.begin();
-	for (; mapit != V.end(); mapit++)
-	{
-		cout << mapit->first << " is " << mapit->second << endl;
-	}
-
 }
